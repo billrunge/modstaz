3498e73161e2eeba47ec173c27979a3e1e1d30e5
billrunge <billrunge@gmail.com>
2019-11-01 09:59:41 -0500

    Pulled the insert row code from the Azure function into the Row library

--- modstaz.Libraries/Row.cs
+++ modstaz.Libraries/Row.cs
@@ -1,35 +1,78 @@
-﻿//using Newtonsoft.Json.Linq;
-//using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace modstaz.Libraries
-//{
-//    public class Row
-//    {
-//       public int StorageAreaId { get; set; }
-//        public async Task<string> InsertRowAsync(JArray fields)
-//        {
-//            string columns = "";
-//            string values = "";
-//            Column column = new Column() { StorageAreaId = StorageAreaId };
-//            string output = "";
+namespace modstaz.Libraries
+{
+    public class Row
+    {
+        public int StorageAreaId { get; set; }
+
+        public async Task<string> InsertRowAsync(JObject fields)
+        {
+            List<ColumnObject> inputColumns = fields.Properties().Select(x => new ColumnObject { DisplayName = x.Name, Value = (string)x.Value }).ToList();
+
+            Column column = new Column() { StorageAreaId = StorageAreaId };
+
+            JArray columnObj = (JArray)JsonConvert.DeserializeObject(await column.GetStorageAreaColumnsAsync());
+
+            List<ColumnObject> columns = columnObj
+                .Where(x => (bool)x["IsEditable"] == true)
+                .Select(x => new ColumnObject { Id = (int)x["ID"], DisplayName = (string)x["DisplayName"] })
+                .ToList();
+
+            List<ColumnObject> updateColumns = (from i in inputColumns
+                                                from c in columns.Where(x => i.DisplayName.ToLower() == x.DisplayName.ToLower() || i.DisplayName == x.Id.ToString())
+                                                select new ColumnObject() { Id = c.Id, DisplayName = c.DisplayName, Value = i.Value }).ToList();
+
+            string columnIds = "";
+            string values = "";
+
+            foreach (ColumnObject c in updateColumns)
+            {
+                columnIds += $" [{ c.Id }],";
+                values += $"'{ c.Value }',";
+            }
+
+            columnIds = columnIds.TrimEnd(',');
+            values = values.TrimEnd(',');
+
+            string sql = $@"
+                INSERT INTO [{ StorageAreaId }ROWS] 
+                ( { columnIds } )
+                VALUES ( { values } )";
+
+            await InsertRowSqlAsync(sql);
+
+            return sql;
+
+        }
+
+        static async Task InsertRowSqlAsync(string sql)
+        {
+            using (SqlConnection connection = new SqlConnection() { ConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING") })
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand(sql, connection);
+                await command.ExecuteNonQueryAsync();
+
+            }
+        }
 
 
-//            foreach (JObject field in fields)
-//            {
-//                output += field. + " ";
-
-//            }
 
 
-//            //return output;
+        class ColumnObject
+        {
+            public int Id { get; set; }
+            public string DisplayName { get; set; }
+            public string Value { get; set; }
+        }
 
 
-
-//            //string sql = $@"
-//            //    INSERT INTO [{ StorageAreaId }Rows] 
-//            //                ({ columns }) 
-//            //    VALUES      ({ values })";
-
-//        }
-//    }
-//}
+    }
+}
--- modstaz/InsertRow.cs
+++ modstaz/InsertRow.cs
@@ -1,18 +1,13 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using modstaz.Libraries;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
-using System.Linq;
-using System.Data.SqlClient;
-using System.Data;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace modstaz.Functions
 {
@@ -30,64 +25,14 @@ namespace modstaz.Functions
             int storageAreaId = data["StorageAreaId"];
             JObject fields = data.Fields;
 
-            List<ColumnObject> inputColumns = fields.Properties().Select(x => new ColumnObject { DisplayName = x.Name, Value = (string)x.Value }).ToList();
 
-            Column column = new Column() { StorageAreaId = storageAreaId };
+            Row row = new Row() { StorageAreaId = storageAreaId };
 
-            JArray columnObj = (JArray)JsonConvert.DeserializeObject(await column.GetStorageAreaColumnsAsync());
-
-            List<ColumnObject> columns = columnObj
-                .Where(x => (bool)x["IsEditable"] == true)
-                .Select(x => new ColumnObject { Id = (int)x["ID"], DisplayName = (string)x["DisplayName"] })
-                .ToList();
-
-            List<ColumnObject> updateColumns = (from i in inputColumns
-                                               from c in columns.Where(x => i.DisplayName.ToLower() == x.DisplayName.ToLower() || i.DisplayName == x.Id.ToString())                                                               
-                                               select new ColumnObject() { Id = c.Id, DisplayName = c.DisplayName, Value = i.Value }).ToList();
-
-            string columnIds = "";
-            string values = "";
-
-            foreach (ColumnObject c in updateColumns)
-            {
-                columnIds += $" [{ c.Id }],";
-                values += $"'{ c.Value }',";
-            }
-
-            columnIds = columnIds.TrimEnd(',');
-            values = values.TrimEnd(',');
-
-            string sql = $@"
-                INSERT INTO [{ storageAreaId }ROWS] 
-                ( { columnIds } )
-                VALUES ( { values } )";
-
-            await InsertRowSqlAsync(sql);
-
-            return new OkObjectResult(sql);
+            return new OkObjectResult(await row.InsertRowAsync(fields));
 
             //return name != null
             //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
             //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
-
-        static async Task InsertRowSqlAsync(string sql)
-        {
-            using (SqlConnection connection = new SqlConnection() { ConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING") })
-            {
-                await connection.OpenAsync();
-                SqlCommand command = new SqlCommand(sql, connection);
-                await command.ExecuteNonQueryAsync();
-
-            }
-        }
-
-    }
-
-    class ColumnObject
-    {
-        public int Id { get; set; }
-        public string DisplayName { get; set; }
-        public string Value { get; set; }
     }
 }
